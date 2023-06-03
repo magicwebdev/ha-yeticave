da3063be3e0a5c6768caac2661744c7f9e5e5755
Aleksandra Khrapkova <axi83@mail.ru>
2023-06-03 20:03:33 +0300

    update index.php

--- index.php
+++ index.php
@@ -14,40 +14,40 @@ $categories = [
 
 $ads = [
   [
-    'title'   => '2014 Rossignol District Snowboard',
-    'project' => 'Доски и лыжи',
-    'price'   => '10999',
-    'img_url' => 'img/lot-1.jpg'
+    'title'    => '2014 Rossignol District Snowboard',
+    'category' => 'Доски и лыжи',
+    'price'    => '10999',
+    'img_url'  => 'img/lot-1.jpg'
   ],
   [
-    'title'   => 'DC Ply Mens 2016/2017 Snowboard',
-    'project' => 'Доски и лыжи',
-    'price'   => '159999',
-    'img_url' => 'img/lot-2.jpg'
+    'title'    => 'DC Ply Mens 2016/2017 Snowboard',
+    'category' => 'Доски и лыжи',
+    'price'    => '159999',
+    'img_url'  => 'img/lot-2.jpg'
   ],
   [
-    'title'   => 'Крепления Union Contact Pro 2015 года размер L/XL',
-    'project' => 'Крепления',
-    'price'   => '8000',
-    'img_url' => 'img/lot-3.jpg'
+    'title'    => 'Крепления Union Contact Pro 2015 года размер L/XL',
+    'category' => 'Крепления',
+    'price'    => '8000',
+    'img_url'  => 'img/lot-3.jpg'
   ],
   [
-    'title'   => 'Ботинки для сноуборда DC Mutiny Charocal',
-    'project' => 'Ботинки',
-    'price'   => '10999',
-    'img_url' => 'img/lot-4.jpg'
+    'title'    => 'Ботинки для сноуборда DC Mutiny Charocal',
+    'category' => 'Ботинки',
+    'price'    => '10999',
+    'img_url'  => 'img/lot-4.jpg'
   ],
   [
-    'title'   => 'Куртка для сноуборда DC Mutiny Charocal',
-    'project' => 'Одежда',
-    'price'   => '7500',
-    'img_url' => 'img/lot-5.jpg'
+    'title'    => 'Куртка для сноуборда DC Mutiny Charocal',
+    'category' => 'Одежда',
+    'price'    => '7500',
+    'img_url'  => 'img/lot-5.jpg'
   ],
   [
-    'title'   => 'Маска Oakley Canopy',
-    'project' => 'Разное',
-    'price'   => '5400',
-    'img_url' => 'img/lot-6.jpg'
+    'title'    => 'Маска Oakley Canopy',
+    'category' => 'Разное',
+    'price'    => '5400',
+    'img_url'  => 'img/lot-6.jpg'
   ]
 ]
 
@@ -101,10 +101,11 @@ $ads = [
           снаряжение.</p>
         <ul class="promo__list">
           <!--заполните этот список из массива категорий-->
-          <?php  ?>
-          <li class="promo__item promo__item--boards">
-            <a class="promo__link" href="pages/all-lots.html">Имя категории</a>
+          <?php foreach($categories as $key => $category): ?>
+          <li class="promo__item promo__item--<?=$key; ?>">
+            <a class="promo__link" href="pages/all-lots.html"><?=$category; ?></a>
           </li>
+          <?php endforeach; ?>
         </ul>
       </section>
       <section class="lots">
@@ -113,17 +114,18 @@ $ads = [
         </div>
         <ul class="lots__list">
           <!--заполните этот список из массива с товарами-->
+          <?php foreach($ads as $ad): ?>
           <li class="lots__item lot">
             <div class="lot__image">
-              <img src="" width="350" height="260" alt="">
+              <img src="<?=$ad['img_url']; ?>" width="350" height="260" alt="<?=$ad['title']; ?>">
             </div>
             <div class="lot__info">
-              <span class="lot__category">Название категории</span>
-              <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
+              <span class="lot__category"><?=$ad['category']; ?></span>
+              <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?=$ad['title']; ?></a></h3>
               <div class="lot__state">
                 <div class="lot__rate">
                   <span class="lot__amount">Стартовая цена</span>
-                  <span class="lot__cost">цена<b class="rub">р</b></span>
+                  <span class="lot__cost"><?=$ad['price']; ?><b class="rub">р</b></span>
                 </div>
                 <div class="lot__timer timer">
                   12:23
@@ -131,6 +133,7 @@ $ads = [
               </div>
             </div>
           </li>
+          <?php endforeach; ?>
         </ul>
       </section>
     </main>
@@ -142,7 +145,7 @@ $ads = [
         <!--заполните этот список из массива категорий-->
         <?php foreach ($categories as $category) : ?>
           <li class="nav__item">
-            <a href="pages/all-lots.html"><?=$category; ?></a>
+            <a href="pages/all-lots.html"><?= $category; ?></a>
           </li>
         <?php endforeach; ?>
       </ul>
